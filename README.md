43ddc6aaebcf6ec4498b47a57ff81d34e0b1918c
needpeeling <martin.gius95@gmail.com>
2022-07-16 19:45:43 +0200

    angular

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# YeahBuddyLightweight
+# Norepnogain
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'yeah-buddy-lightweight'`, () => {
+  it(`should have as title 'norepnogain'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('yeah-buddy-lightweight');
+    expect(app.title).toEqual('norepnogain');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('yeah-buddy-lightweight app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('norepnogain app is running!');
   });
 });
