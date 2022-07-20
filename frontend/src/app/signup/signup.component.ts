f04b94481bbfe49a415abf387c5763365cb6778b
needpeeling <martin.gius95@gmail.com>
2022-07-20 15:07:41 +0200

    reworked sign up validations

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -13,25 +13,47 @@ export class SignupComponent implements OnInit {
 
   signupForm = this.fb.group({
     mail: ['', [Validators.required, Validators.email]],
-    name: ['', [Validators.required]],
+    name: ['', [Validators.required, Validators.minLength(3)]],
     password: ['', [Validators.required, Validators.minLength(6)]],
-    password_confirm: ['', [Validators.required, Validators.minLength(6)]]
-  })
+    password_confirm: ['', [Validators.required]]
+  },
+  {  
+    validators: this.matchPassword('password', 'password_confirm')
+  }
+  )
 
   constructor(private  fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
-
-
   submitSignUpForm() {
     console.log(this.signupForm.value);
   }
 
-  get mail() { return this.signupForm.get('mail'); }
-  get name() { return this.signupForm.get('name'); }
-  get password() { return this.signupForm.get('password'); }
-  get password_confirm() { return this.signupForm.get('password_confirm'); }
+/**
+ * If the password and password_confirm fields don't match, set the password_confirm field's errors to
+ * { matchPassword: true }
+ * @param {any} password - The name of the first password field.
+ * @param {any} password_confirm - The name of the form control that you want to validate.
+ * @returns A function that takes a FormGroup as an argument.
+ */
+  matchPassword(password:any, password_confirm:any) {
+    return (fg: FormGroup) => {
+      const passwordControl = fg.controls[password];
+      const password_confirmControl = fg.controls[password_confirm];
 
+      if(password_confirmControl.errors && !password_confirmControl.errors['matchPassword'])
+        return;
+
+      if(passwordControl.value !== password_confirmControl.value) {
+        password_confirmControl.setErrors({ matchPassword: true });
+      }
+      else {
+        password_confirmControl.setErrors(null);
+      }
+    }
+  }
+
+  get form() { return this.signupForm.controls; }
 }
